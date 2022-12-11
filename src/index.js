b6a100b067ae466f1b282b322b534b20e97b88aa
zClapr <48451623+zLuminate@users.noreply.github.com>
2022-12-11 15:51:44 +0100

    gh-pages finally work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 import Topbar from './components/Topbar';
@@ -9,29 +8,48 @@ import Error from './pages/Error';
 import Login from './pages/Login';
 import StudentCouncil from './pages/subs/StudentCouncil';
 
-const router = createBrowserRouter([
-  {
-    path: '/AICS-Student-Dashboard/',
-    element: <Home />,
-    errorElement: <Error />,
-  },
-  {
-    path: '/AICS-Student-Dashboard/login',
-    element: <Login />,
-  },
-  {
-    path: '/AICS-Student-Dashboard/student-council',
-    element: <StudentCouncil />,
-  }
-])
+import { Routes, Route, HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Topbar />
-    <RouterProvider router={router}></RouterProvider>
+    <HashRouter>
+      <Topbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/error" element={<Error />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/student-council" element={<StudentCouncil />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
+
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/AICS-Student-Dashboard',
+//     element: <Home />,
+//     errorElement: <Error />,
+//   },
+//   {
+//     path: '/AICS-Student-Dashboard/login',
+//     element: <Login />,
+//   },
+//   {
+//     path: '/AICS-Student-Dashboard/student-council',
+//     element: <StudentCouncil />,
+//   }
+// ])
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Topbar />
+//     <RouterProvider router={router}></RouterProvider>
+//   </React.StrictMode>
+// );
 
 // To send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
