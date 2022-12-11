a884474d9e1fcd504ccf93b4ad868d85a7a29f78
zClapr <48451623+zLuminate@users.noreply.github.com>
2022-12-11 14:04:00 +0100

    github pages
    please work

--- src/pages/Homepage/Home.js
+++ src/pages/Homepage/Home.js
@@ -1,15 +1,17 @@
 import './Home.css'
 
-
 function Home() {
   return (
-    <div className="Home">
-	  <span>
-		<h1>Welcome!</h1>
-	  	<p>Where are we off to today?</p>
-	  </span>
-
-	</div>
+	<div className="Home">
+	<span>
+	  <h1>Welcome!</h1>
+		<p>Where are we off to today?</p>
+	</span>
+	<h1>help</h1>
+	<span style={{backgroundColor: "#000000"}}>
+		<h1> E </h1>
+	</span>
+  </div>
   );
   }
 
